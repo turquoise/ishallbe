004ca8f0979f04537c24278525beea8be7adccfb
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-29 21:46:10 +0000

    changed color to header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,18 +4,18 @@ import Helmet from 'react-helmet';
 import AppBar from 'material-ui/AppBar';
 import { Grid, Row, Col } from 'react-flexbox-grid'
 import FlatButton from 'material-ui/FlatButton';
-import Regina from  './assets/images/reginajonas.png';
+import Regina from  './assets/images/reginajonas2.png';
 import './index.css';
 
 const mystyles = {
   container: {
-    background: '#957a5f',
+    background: '#030506',
     marginBottom: '1.45rem'
   },
   main: {
     margin: '0 auto',
     maxWidth: 960,
-    height: 200,
+    height: 280,
     padding: '1.45rem 1.0875rem',
   },
   h1: {
@@ -26,9 +26,12 @@ const mystyles = {
     color: 'black'
   },
   Link: {
-    color: 'black',
-    fontSize: '60px',
+    color: '#aa8956',
+    fontSize: '70px',
     textDecoration: 'none'
+  },
+  text: {
+    color: '#aa8956',
   },
   img: {
     width: '100%',
@@ -52,11 +55,15 @@ const Header = () => (
         </Col>
         <Col xs={6} sm={4} md={4} lg={4}>  
           <div style={mystyles.img}>
-            <img width="240px" src={Regina} alt="Regina Jonas" />
+          <a href="http://ravblog.ccarnet.org/2014/08/reflections-rabbiner-regina-jonas-mission/" target="_blank">
+            <img width="250px" src={Regina} alt="Regina Jonas" />
+            </a>
           </div>
         </Col>
         <Col md={2} lg={2}>
-          <h3 className="myheading">Regina Jonas (1902-1944)</h3>
+          <div style={mystyles.text} >
+          <h3 className="myheading" >Regina Jonas (1902-1944)</h3>
+          </div>
         </Col>
         </Row>
       </Grid>
