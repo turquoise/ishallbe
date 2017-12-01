6169f3ee854c6394efdae04c87921b905f3a208b
M Gosschalk <monica.gosschalk@btinternet.com>
2017-12-01 19:32:25 +0000

    added firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
         path: `${__dirname}/src/`,
       }
     },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: `fupty2cdhse7`,
+    //     accessToken: `e975e1e367e750f9d2146965eac631c66297e45275a4f2933a958188e0aa9e85`,
+    //   }
+    // },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typography`,
     `gatsby-plugin-glamor`,
