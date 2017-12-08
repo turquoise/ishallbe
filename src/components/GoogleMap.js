2f067a27b71d2e1715da947ddd17ce7f22b04df2
M Gosschalk <monica.gosschalk@btinternet.com>
2017-12-08 18:39:08 +0000

    removed map key

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -52,9 +52,6 @@ class GoogleMap extends React.Component {
     return (
       <div style={style.map}>
         <GoogleMapReact
-          bootstrapURLKeys={{
-            key: 'AIzaSyD-oU-btZC66MPOfAo1Z_s08kd8g95SqaU',
-          }}
           defaultCenter={ this.props.center }
           defaultZoom={ this.props.zoom }>
           <AnyReactComponent 
